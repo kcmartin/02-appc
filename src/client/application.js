04f2c6690b7047b822738862535be66545c4e9e3
kcmartin <kcmartin@users.noreply.github.com>
2016-06-08 12:51:45 -0700

    Finish skeleton
    Clean-up structure and remove test files

--- src/client/application.js
+++ src/client/application.js
@@ -1,11 +1,1 @@
-import {blegh} from "shared/test";
-import $ from "jquery";
 import "./application.scss";
-
-blegh();
-
-$("body").html("Pretty rad");
-
-if (module.hot) {
-    module.hot.accept();
-}
